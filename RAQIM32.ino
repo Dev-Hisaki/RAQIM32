204fd3d972ae35324cb145354c4c333d5c41acf4
Muhamad Haidar Al-Farruq <62578000+Dev-Hisaki@users.noreply.github.com>
2026-03-02 05:48:40 +0700

    Init

---
+++ RAQIM32.ino
@@ -0,0 +1,20 @@
+/*
+===============To Do List==================
+1. Configure Read/Write authority
+2. Sending CO2, humidity, and temperature data in a room to a Firebase Real Time database.
+3. Sending data to database with a time range that can decided by admin.
+4. Adding error handling when attempting to send/receive data (Ex. Buzzer/LED/LCD).
+5. Getting data from firebase to webapp
+6. Display on dashboard (webapp)
+===========================================
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
